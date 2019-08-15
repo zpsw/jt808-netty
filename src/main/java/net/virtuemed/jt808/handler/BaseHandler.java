230c08bbd7f95fa7ce4882f655c108e1fecf1d36
zpsw <zpswing@gmail.com>
2019-08-15 09:49:27 +0800

    idle事件细化处理

--- src/main/java/net/virtuemed/jt808/handler/BaseHandler.java
+++ src/main/java/net/virtuemed/jt808/handler/BaseHandler.java
@@ -3,6 +3,7 @@ package net.virtuemed.jt808.handler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
@@ -54,8 +55,16 @@ public abstract class BaseHandler<T> extends SimpleChannelInboundHandler<T> {
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if (evt instanceof IdleStateEvent) {
-            log.warn("客户端{}读取超时，自动断开", ctx.channel().remoteAddress());
-            ctx.close();
+            //此实例项目只设置了读取超时时间,可以通过state分别做处理,一般服务端在这里关闭连接节省资源，客户端发送心跳维持连接
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                log.warn("客户端{}读取超时,关闭连接", ctx.channel().remoteAddress());
+                ctx.close();
+            } else if (state == IdleState.WRITER_IDLE) {
+                log.warn("客户端{}写入超时", ctx.channel().remoteAddress());
+            }else if(state == IdleState.ALL_IDLE){
+                log.warn("客户端{}读取写入超时", ctx.channel().remoteAddress());
+            }
         } else {
             super.userEventTriggered(ctx, evt);
         }
